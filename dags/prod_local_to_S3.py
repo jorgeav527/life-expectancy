9082a57c824055d28b86b21c3612a23252f64ea9
jorgeav527 <jorgeav527@gmail.com>
2022-12-20 23:29:22 -0500

    get very carefull about the env variables os.environ.get('AWS_ENDPOINT_URL')

--- dags/prod_local_to_S3.py
+++ dags/prod_local_to_S3.py
@@ -5,7 +5,7 @@ import os
 linode_obj_config = {
     "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
     "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
-    "endpoint_url": os.environ.get("ENDPOINT_URL"),
+    "endpoint_url": os.environ.get("AWS_ENDPOINT_URL"),
 }
 
 # boto service
