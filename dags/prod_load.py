bb1f744a873ea84313427948dd4dfd90907dff3c
jorgeav527 <jorgeav527@gmail.com>
2022-12-20 22:33:45 -0500

    dags and docker compose refactoring to os.environ.get because in docker cointerner it works

--- dags/prod_load.py
+++ dags/prod_load.py
@@ -2,18 +2,15 @@ import os
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
-
-load_dotenv()
 
 from prod_local_to_S3 import client
 from prod_dir_creation import DATOS_PROCESADOS
 
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
 
 
 def renombrar_columnas_paises():
--- dags/prod_local_to_S3.py
+++ dags/prod_local_to_S3.py
@@ -1,15 +1,11 @@
 import boto3
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # linode access keys to the bucket
 linode_obj_config = {
-    "aws_access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
-    "aws_secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-    "endpoint_url": os.getenv("ENDPOINT_URL"),
+    "aws_access_key_id": os.environ.get("AWS_ACCESS_KEY_ID"),
+    "aws_secret_access_key": os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    "endpoint_url": os.environ.get("ENDPOINT_URL"),
 }
 
 # boto service
